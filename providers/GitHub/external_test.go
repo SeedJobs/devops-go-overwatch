5257465c96b75e44d277c42599f723bd2a5b6bec
Sean Maraciniak <sean@beamery.com>
2018-03-14 11:45:33 +0000

    Basic test for interface

--- providers/GitHub/external_test.go
+++ providers/GitHub/external_test.go
@@ -1,1 +1,18 @@
 package github_test
+
+import (
+	"testing"
+
+	overwatch "github.com/SeedJobs/devops-go-overwatch"
+	github "github.com/SeedJobs/devops-go-overwatch/providers/GitHub"
+)
+
+func TestInterface(t *testing.T) {
+	man, err := github.NewManager()
+	if err != nil {
+		t.Fatal("Unable to create manager")
+	}
+	if _, ok := man.(overwatch.IamPolicyManager); !ok {
+		t.Fatal("Does not implement the overwatch IPolicyManager")
+	}
+}
