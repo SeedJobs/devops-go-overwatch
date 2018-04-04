b1e13ba3c907fd3b58b8063537f38e273412c702
Sean Maraciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2018-04-04 13:42:23 +0100

    Adding the ability to Resync to backend storage (#5)
    * Adding the start of the Github work
    
    * Basic test for interface
    
    * Adding some updates as to the inner workings of the application
    
    * Adding updates
    
    * Project Discovery with addition of branch protection discovery
    
    * Making pagination a nice power of 2
    
    * Doing the thing where I think about tests to build
    
    * Fixed issue with not being able to list repos
    
    * Make sure we clean up after ourselves
    
    * Adding some further updates
    
    * Doing the thing
    
    * Adding test file
    
    * Allowing for dynamic transformers
    
    * Adding transformers
    
    * Fixing merge conflicts
    
    * Making some sick updates
    
    * Adding my changes
    
    * Adding some updates
    
    * Fixing merge conflicts
    
    * Fixied the modified resources logic
    
    * Fixing some silly issues that == isn't good enough
    
    * Fixing grammer
    
    * Adding updates
    
    * Making things easier to read
    
    * Fixing struct packing
    
    * Taking an interesting turn
    
    * Moving the delete key
    
    * Adding some code styling
    
    * Updating the current caching strat
    
    * Adding updated items
    
    * Adding all my work thus far
    
    * Doing a lot of work
    
    * Don't really like it but it works
    
    * Adding updates
    
    * Fixing accidental merges
    
    * Allowing runtime defined RemoteSync
    
    * Make sure we querry each branch
    
    * Making sure that we don't crash the process by panicing

--- providers/GitHub/manager.go
+++ providers/GitHub/manager.go
@@ -3,14 +3,18 @@ package github
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 	"reflect"
+	"time"
 
 	overwatch "github.com/SeedJobs/devops-go-overwatch"
 	"github.com/SeedJobs/devops-go-overwatch/providers/default"
 	gogithub "github.com/google/go-github/github"
 	"golang.org/x/oauth2"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type manager struct {
@@ -46,7 +50,7 @@ func (m *manager) LoadConfiguration(conf overwatch.IamManagerConfig) error {
 	} else {
 		return fmt.Errorf("GITHUB_ORG was not defined in conf additional map")
 	}
-	return m.readFromDisc()
+	return m.readFromDisk()
 }
 
 func (m *manager) Resources() []overwatch.IamResource {
@@ -62,31 +66,38 @@ func (m *manager) Resources() []overwatch.IamResource {
 // ListModifiedResources will examine resources loaded from Github and check
 // them against the expected store configuration.
 func (m *manager) ListModifiedResources() ([]overwatch.IamResource, error) {
-	modified := []overwatch.IamResource{}
-	for _, obj := range m.fetchOrgProjects() {
-		// early exit if possible
-		if _, exist := m.resources[obj.GetType()]; !exist {
-			break
-		}
-		// Check if our stored config matches what is contained
-		// inside the current resource
-		store, exist := m.resources[obj.GetType()][obj.GetName()]
-		switch {
-		case !exist:
-			// Do nothing
-		// Had to use DeepEqual as the standard equal just simply doesn't work in this case
-		case !reflect.DeepEqual(store, obj):
-			modified = append(modified, obj)
-		}
+	collection, err := m.fetchOrgProjects()
+	if err != nil {
+		return nil, err
 	}
-	return modified, nil
+	notcached, modified := m.seperateLists(collection)
+	return append(notcached, modified...), nil
 }
 
 func (m *manager) Resync() ([]overwatch.IamResource, error) {
-	return nil, overwatch.ErrNotImplemented
+	items := []overwatch.IamResource{}
+	if time.Now().After(m.base.Expire) {
+		collection, err := m.fetchOrgProjects()
+		if err != nil {
+			return nil, err
+		}
+		notcached, modified := m.seperateLists(collection)
+		for _, item := range notcached {
+			if _, exist := m.resources[item.GetType()]; !exist {
+				m.resources[item.GetType()] = map[string]overwatch.IamResource{}
+			}
+			m.resources[item.GetType()][item.GetName()] = item
+		}
+		if err := m.writeToDisk(); err != nil {
+			return nil, err
+		}
+		items = append(items, modified...)
+		m.base.Expire = time.Now().Add(m.base.Conf.TimeOut)
+	}
+	return items, nil
 }
 
-func (m *manager) fetchOrgProjects() []overwatch.IamResource {
+func (m *manager) fetchOrgProjects() ([]overwatch.IamResource, error) {
 	opt := &gogithub.RepositoryListByOrgOptions{
 		ListOptions: gogithub.ListOptions{
 			PerPage: 64,
@@ -97,7 +108,7 @@ func (m *manager) fetchOrgProjects() []overwatch.IamResource {
 		// This call is limited by the token issuer as it can only see what the issuer can see inside the org
 		repos, resp, err := m.client.Repositories.ListByOrg(context.Background(), m.organisation, opt)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		for _, pro := range repos {
 			repo := project{
@@ -105,17 +116,24 @@ func (m *manager) fetchOrgProjects() []overwatch.IamResource {
 				Public:    !pro.GetPrivate(),
 				Protected: []string{},
 			}
-			branches, _, err := m.client.Repositories.ListBranches(context.Background(),
-				pro.GetOwner().GetLogin(),
-				pro.GetName(),
-				&gogithub.ListOptions{})
-			if err != nil {
-				panic(err)
-			}
-			for _, branch := range branches {
-				if branch.GetProtected() {
-					repo.Protected = append(repo.Protected, branch.GetName())
+			branchOpts := &gogithub.ListOptions{}
+			for {
+				branches, branchresp, err := m.client.Repositories.ListBranches(context.Background(),
+					pro.GetOwner().GetLogin(),
+					pro.GetName(),
+					branchOpts)
+				if err != nil {
+					return nil, err
 				}
+				for _, branch := range branches {
+					if branch.GetProtected() {
+						repo.Protected = append(repo.Protected, branch.GetName())
+					}
+				}
+				if branchresp.NextPage == 0 {
+					break
+				}
+				branchOpts.Page = branchresp.NextPage
 			}
 			allRepos = append(allRepos, repo)
 		}
@@ -124,10 +142,29 @@ func (m *manager) fetchOrgProjects() []overwatch.IamResource {
 		}
 		opt.Page = resp.NextPage
 	}
-	return allRepos
+	return allRepos, nil
 }
 
-func (m *manager) readFromDisc() error {
+func (m *manager) seperateLists(collection []overwatch.IamResource) ([]overwatch.IamResource, []overwatch.IamResource) {
+	notcached, modified := []overwatch.IamResource{}, []overwatch.IamResource{}
+	for _, item := range collection {
+		if _, exist := m.resources[item.GetType()]; !exist {
+			notcached = append(notcached, item)
+			// early exit on the loop
+			continue
+		}
+		obj, exist := m.resources[item.GetType()][item.GetName()]
+		switch {
+		case !exist:
+			notcached = append(notcached, item)
+		case !reflect.DeepEqual(item, obj):
+			modified = append(modified, item)
+		}
+	}
+	return notcached, modified
+}
+
+func (m *manager) readFromDisk() error {
 	// Remove items for the internal cache
 	for key, _ := range m.resources {
 		delete(m.resources, key)
@@ -143,6 +180,30 @@ func (m *manager) readFromDisc() error {
 			m.resources[obj.GetType()] = map[string]overwatch.IamResource{}
 		}
 		m.resources[obj.GetType()][obj.GetName()] = obj
+	}
+	return nil
+}
+
+func (m *manager) writeToDisk() error {
+	for key, items := range m.resources {
+		dir := path.Join(m.base.Storer.GetPath(), "Github", m.organisation, key+"s")
+		data := []overwatch.IamResource{}
+		for _, obj := range items {
+			data = append(data, obj)
+		}
+		buff, err := yaml.Marshal(&data)
+		if err != nil {
+			return err
+		}
+		f := path.Join(dir, key+".yml")
+		// Removing any old file as need just to remove it
+		if _, err := os.Stat(f); !os.IsNotExist(err) {
+			os.Remove(f)
+		}
+		// Write the updated file
+		if err := ioutil.WriteFile(f, buff, 0644); err != nil {
+			return err
+		}
 	}
 	return nil
 }
